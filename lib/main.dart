be0ca5fc668b66713448af0284b14028b15a487b
Seth <sethgoldin27@gmail.com>
2020-05-07 00:39:25 -0400

    swap to flutter, add file upload

---
+++ lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:peardrop/src/home.dart';
+import 'package:flutter/widgets.dart';
+
+void main() => runApp(new HomePage());
---
+++ lib/main_desktop.dart
@@ -0,0 +1,8 @@
+import 'package:peardrop/src/home.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
+  runApp(new HomePage());
+}
