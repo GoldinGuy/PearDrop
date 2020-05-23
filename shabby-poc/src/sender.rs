1bc3ed59878a63aa91c583fb0e06a622d984420f
anirudhb <anirudhb@users.noreply.github.com>
2020-05-23 12:53:28 -0700

    Proper SenderPacket implementation

--- shabby-poc/src/sender.rs
+++ shabby-poc/src/sender.rs
@@ -17,7 +17,7 @@ pub trait SenderExtension: std::fmt::Debug {}
 #[derive(Debug)]
 pub struct SenderPacket {
     filename: String,
-    mime_type: String,
+    mimetype: String,
     extensions: Vec<Box<dyn SenderExtension>>,
     data_len: u64,
 }
@@ -41,18 +41,85 @@ impl Marshal for SenderPacket {
         if u16::from(mimetype_len) > MAX_MIMETYPE_LEN {
             return Err(SenderPacketError::MIMETypeLengthExceeded(mimetype_len));
         }
+
+        // Read filename and MIME type
         let mut filename_v = vec![0; u16::from(filename_len) as usize];
         r.read_exact(&mut filename_v)?;
+        let filename = match String::from_utf8(filename_v) {
+            Ok(s) => s,
+            Err(_) => return Err(SenderPacketError::InvalidFilename),
+        };
         let mut mimetype_v = vec![0; u16::from(mimetype_len) as usize];
         r.read_exact(&mut mimetype_v)?;
-        unimplemented!()
+        let mimetype = match String::from_utf8(mimetype_v) {
+            Ok(s) => s,
+            Err(_) => return Err(SenderPacketError::InvalidMIMEType),
+        };
+
+        // Read extensions
+        let exts_len = r.read_u8()?;
+        let exts = Self::read_exts(r, exts_len)?;
+
+        // Read data lenth
+        let data_len = r.read_u64::<BE>()?;
+
+        Ok(Self::new(filename, mimetype, exts, data_len))
     }
 
     /**
      * Writes this SenderPacket to the given writer.
      */
     fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error> {
-        unimplemented!()
+        use byteorder::{WriteBytesExt, BE};
+        let ext_len = self.extensions.len();
+        if ext_len != 0 {
+            unimplemented!();
+        }
+
+        // Write filename and MIME type
+        let triple_byte = ((self.filename.len() as u32) << 12) | self.mimetype.len() as u32;
+        w.write_u24::<BE>(triple_byte)?;
+        w.write_all(&self.filename.as_bytes())?;
+        w.write_all(&self.mimetype.as_bytes())?;
+
+        // Write extensions
+        w.write_u8(ext_len as u8)?;
+
+        // Write data_len
+        w.write_u64::<BE>(self.data_len)?;
+
+        Ok(())
+    }
+}
+
+impl SenderPacket {
+    /**
+     * Creates a new SenderPacket using the given name, MIME type, extensions
+     * and data length.
+     */
+    pub fn new(
+        filename: String,
+        mimetype: String,
+        extensions: Vec<Box<dyn SenderExtension>>,
+        data_len: u64,
+    ) -> Self {
+        Self {
+            filename,
+            mimetype,
+            extensions,
+            data_len,
+        }
+    }
+
+    fn read_exts(
+        r: &mut dyn std::io::Read,
+        exts_len: u8,
+    ) -> Result<Vec<Box<dyn SenderExtension>>, SenderPacketError> {
+        if exts_len != 0 {
+            unimplemented!()
+        } else {
+            Ok(Vec::new())
+        }
     }
 }
 
@@ -70,22 +137,10 @@ pub enum SenderPacketError {
     FilenameLengthExceeded(ux::u12),
     #[error("MIME type length exceeded {} bytes (got {0} bytes)", MAX_MIMETYPE_LEN)]
     MIMETypeLengthExceeded(ux::u12),
+    #[error("Filename is not valid UTF-8")]
+    InvalidFilename,
+    #[error("MIME type is not valid UTF-8")]
+    InvalidMIMEType,
     #[error("IO error: {0}")]
     IO(#[from] std::io::Error),
-}
-
-impl SenderPacket {
-    pub fn new(
-        filename: String,
-        mime_type: String,
-        extensions: Vec<Box<dyn SenderExtension>>,
-        data_len: u64,
-    ) -> Self {
-        Self {
-            filename,
-            mime_type,
-            extensions,
-            data_len,
-        }
-    }
 }
