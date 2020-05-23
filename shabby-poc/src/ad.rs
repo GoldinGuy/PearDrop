3a33dbd9e201e44553387f11be50ff9df60c84aa
anirudhb <anirudhb@users.noreply.github.com>
2020-05-23 12:12:20 -0700

    Initial sender/ad packet implementation

--- shabby-poc/src/ad.rs
+++ shabby-poc/src/ad.rs
@@ -1,3 +1,6 @@
+use super::Marshal;
+use thiserror::Error;
+
 /**
  * Advertisement packet extension.
  *
@@ -5,12 +8,68 @@
  *
  * TODO: Add Error type.
  */
-pub trait AdExtension: super::Extension {}
+pub trait AdExtension: std::fmt::Debug {}
 
 /**
  * Advertisement packet.
  * See the core protocol.
  */
+#[derive(Debug)]
 pub struct AdPacket {
     extensions: Vec<Box<dyn AdExtension>>,
+}
+
+impl Marshal for AdPacket {
+    type Error = AdPacketError;
+
+    /**
+     * Reads an AdPacket from the given reader.
+     */
+    fn read(r: &mut dyn std::io::Read) -> Result<Self, Self::Error> {
+        use byteorder::ReadBytesExt;
+        /* ext len */
+        let ext_len = r.read_u8()?;
+        let exts = Self::read_exts(r, ext_len)?;
+        Ok(Self::new(exts))
+    }
+
+    /**
+     * Writes an AdPacket to the given writer.
+     */
+    fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error> {
+        use byteorder::WriteBytesExt;
+        let ext_len = self.extensions.len() as u8;
+        if ext_len != 0 {
+            unimplemented!();
+        }
+        w.write_u8(ext_len)?;
+        Ok(())
+    }
+}
+
+impl AdPacket {
+    /**
+     * Creates a new AdPacket using the given extensions.
+     */
+    pub fn new(extensions: Vec<Box<dyn AdExtension>>) -> Self {
+        Self { extensions }
+    }
+
+    fn read_exts(
+        _r: &mut dyn std::io::Read,
+        ext_len: u8,
+    ) -> Result<Vec<Box<dyn AdExtension>>, AdPacketError> {
+        // TODO: implement
+        if ext_len != 0 {
+            unimplemented!()
+        } else {
+            Ok(Vec::new())
+        }
+    }
+}
+
+#[derive(Error, Debug)]
+pub enum AdPacketError {
+    #[error("{0}")]
+    IO(#[from] std::io::Error),
 }
--- shabby-poc/src/sender.rs
+++ shabby-poc/src/sender.rs
@@ -1,3 +1,6 @@
+use super::Marshal;
+use thiserror::Error;
+
 /**
  * Sender packet extension.
  *
@@ -5,15 +8,84 @@
  *
  * TODO: Add Error type.
  */
-pub trait SenderExtension: super::Extension {}
+pub trait SenderExtension: std::fmt::Debug {}
 
 /**
  * Sender packet.
  * See the core protocol.
  */
+#[derive(Debug)]
 pub struct SenderPacket {
     filename: String,
     mime_type: String,
     extensions: Vec<Box<dyn SenderExtension>>,
     data_len: u64,
+}
+
+impl Marshal for SenderPacket {
+    type Error = SenderPacketError;
+
+    /**
+     * Reads a SenderPacket from the given reader.
+     */
+    fn read(r: &mut dyn std::io::Read) -> Result<Self, Self::Error> {
+        use byteorder::{ReadBytesExt, BE};
+        use ux::u12;
+        let triple_byte = r.read_u24::<BE>()?;
+        // Take out filename length and MIME type length
+        let filename_len = u12::new(((triple_byte & 0xfff000) >> 12) as u16);
+        let mimetype_len = u12::new((triple_byte & 0xfff) as u16);
+        if u16::from(filename_len) > MAX_FILENAME_LEN {
+            return Err(SenderPacketError::FilenameLengthExceeded(filename_len));
+        }
+        if u16::from(mimetype_len) > MAX_MIMETYPE_LEN {
+            return Err(SenderPacketError::MIMETypeLengthExceeded(mimetype_len));
+        }
+        let mut filename_v = vec![0; u16::from(filename_len) as usize];
+        r.read_exact(&mut filename_v)?;
+        let mut mimetype_v = vec![0; u16::from(mimetype_len) as usize];
+        r.read_exact(&mut mimetype_v)?;
+        unimplemented!()
+    }
+
+    /**
+     * Writes this SenderPacket to the given writer.
+     */
+    fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error> {
+        unimplemented!()
+    }
+}
+
+/// Maximum length of the filename in a SenderPacket.
+pub const MAX_FILENAME_LEN: u16 = 4084; /* see kjetilkjeka/ux#5 */
+/// Maximum length of the MIME type in a SenderPacket.
+pub const MAX_MIMETYPE_LEN: u16 = 4084; /* see kjetilkjeka/ux#5 */
+
+/**
+ * Error while constructing a SenderPacket.
+ */
+#[derive(Error, Debug)]
+pub enum SenderPacketError {
+    #[error("Filename length exceeded {} bytes (got {0} bytes)", MAX_FILENAME_LEN)]
+    FilenameLengthExceeded(ux::u12),
+    #[error("MIME type length exceeded {} bytes (got {0} bytes)", MAX_MIMETYPE_LEN)]
+    MIMETypeLengthExceeded(ux::u12),
+    #[error("IO error: {0}")]
+    IO(#[from] std::io::Error),
+}
+
+impl SenderPacket {
+    pub fn new(
+        filename: String,
+        mime_type: String,
+        extensions: Vec<Box<dyn SenderExtension>>,
+        data_len: u64,
+    ) -> Self {
+        Self {
+            filename,
+            mime_type,
+            extensions,
+            data_len,
+        }
+    }
 }
