5625a3eb3e81b14a9d701ce84fdc063d1353ce1a
anirudhb <anirudhb@users.noreply.github.com>
2020-05-24 10:36:26 -0700

    Add CODEOWNERS

--- peardrop_flutter/lib/main.dart
+++ peardrop_flutter/lib/main.dart
@@ -1,4 +1,27 @@
-import 'package:peardrop/src/home.dart';
+import 'package:flutter/material.dart';
+import 'package:peardrop/src/circle.dart';
+import 'package:peardrop/src/devices.dart';
+import 'package:peardrop/src/file_upload.dart';
 import 'package:flutter/widgets.dart';
+import 'package:peardrop/src/settings.dart';
 
-void main() => runApp(new HomePage());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PearDrop',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff6b9080),
+      ),
+      routes: {
+        '/circle': (_) => CirclePage(),
+        '/devices': (_) => DevicesPage(),
+        '/settings': (_) => SettingsScreen(),
+      },
+      home: DevicesPage(),
+    );
+  }
+}
--- peardrop_flutter/lib/main_desktop.dart
+++ peardrop_flutter/lib/main_desktop.dart
@@ -1,8 +1,9 @@
-import 'package:peardrop/src/home.dart';
+import 'package:peardrop/src/circle.dart';
+import 'package:peardrop/src/devices.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 
 void main() {
   debugDefaultTargetPlatformOverride = TargetPlatform.fuchsia;
-  runApp(new HomePage());
+  runApp(new DevicesPage());
 }
