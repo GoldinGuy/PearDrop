e509708b440e5d464e2dbd1c009c56b486db3fa7
anirudhb <anirudhb@users.noreply.github.com>
2020-06-05 15:37:17 -0700

    Add TCP extensions to AdPacket

--- shabby-poc/src/ad.rs
+++ shabby-poc/src/ad.rs
@@ -1,14 +1,27 @@
-use super::Marshal;
+use super::{Marshal, Size};
 use thiserror::Error;
 
 /**
- * Advertisement packet extension.
- *
- * Empty as it is implemented by other structs.
- *
- * TODO: Add Error type.
+ * TCP acknowledgement packet extension.
+ * See network extensions.
  */
-pub trait AdExtension: std::fmt::Debug {}
+#[derive(Debug, Clone)]
+pub struct TCPAdExtension {
+    pub ad_port: u16,
+}
+
+impl Size for TCPAdExtension {
+    const SIZE: u8 = std::mem::size_of::<u16>() as _;
+}
+
+/**
+ * Type of the advertisement packet extension.
+ */
+#[repr(u8)]
+#[derive(num_derive::FromPrimitive, Debug, Copy, Clone, PartialEq, Eq)]
+pub enum AdExtensionType {
+    TCP,
+}
 
 /**
  * Advertisement packet.
@@ -16,7 +29,22 @@ pub trait AdExtension: std::fmt::Debug {}
  */
 #[derive(Debug)]
 pub struct AdPacket {
-    extensions: Vec<Box<dyn AdExtension>>,
+    pub extensions: AdExtensions,
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct AdExtensions {
+    pub tcp: Option<TCPAdExtension>,
+}
+
+impl AdExtensions {
+    pub fn len(&self) -> u8 {
+        let mut len = 0;
+        if self.tcp.is_some() {
+            len += 1;
+        }
+        len
+    }
 }
 
 impl Marshal for AdPacket {
@@ -39,10 +67,8 @@ impl Marshal for AdPacket {
     fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error> {
         use byteorder::WriteBytesExt;
         let ext_len = self.extensions.len() as u8;
-        if ext_len != 0 {
-            unimplemented!();
-        }
         w.write_u8(ext_len)?;
+        Self::write_exts(w, &self.extensions)?;
         Ok(())
     }
 }
@@ -51,20 +77,40 @@ impl AdPacket {
     /**
      * Creates a new AdPacket using the given extensions.
      */
-    pub fn new(extensions: Vec<Box<dyn AdExtension>>) -> Self {
+    pub fn new(extensions: AdExtensions) -> Self {
         Self { extensions }
     }
 
-    fn read_exts(
-        _r: &mut dyn std::io::Read,
-        ext_len: u8,
-    ) -> Result<Vec<Box<dyn AdExtension>>, AdPacketError> {
-        // TODO: implement
-        if ext_len != 0 {
-            unimplemented!()
-        } else {
-            Ok(Vec::new())
+    fn read_exts(r: &mut dyn std::io::Read, ext_len: u8) -> Result<AdExtensions, AdPacketError> {
+        use byteorder::{ReadBytesExt, BE};
+        use num_traits::FromPrimitive;
+        let mut exts = AdExtensions::default();
+        for _ in 0..ext_len {
+            let ty = r.read_u8()?;
+            let ty = AdExtensionType::from_u8(ty).ok_or(AdExtensionError::InvalidType(ty))?;
+            use AdExtensionType::*;
+            match ty {
+                TCP => {
+                    let len = r.read_u8()?;
+                    if len != TCPAdExtension::SIZE {
+                        return Err(AdExtensionError::InvalidSize(ty, len).into());
+                    }
+                    let port = r.read_u16::<BE>()?;
+                    exts.tcp = Some(TCPAdExtension { ad_port: port });
+                }
+            }
         }
+        Ok(exts)
+    }
+
+    fn write_exts(w: &mut dyn std::io::Write, exts: &AdExtensions) -> Result<(), AdPacketError> {
+        use byteorder::{WriteBytesExt, BE};
+        if let Some(ref tcp) = exts.tcp {
+            w.write_u8(AdExtensionType::TCP as _)?;
+            w.write_u8(TCPAdExtension::SIZE)?;
+            w.write_u16::<BE>(tcp.ad_port)?;
+        }
+        Ok(())
     }
 }
 
@@ -72,26 +118,84 @@ impl AdPacket {
 pub enum AdPacketError {
     #[error("{0}")]
     IO(#[from] std::io::Error),
+    #[error("{0}")]
+    AdExtension(#[from] AdExtensionError),
+}
+
+#[derive(Error, Debug)]
+pub enum AdExtensionError {
+    #[error("Invalid extension type {0}")]
+    InvalidType(u8),
+    #[error("Invalid extension size for {0:#?} (got {1})")]
+    InvalidSize(AdExtensionType, u8),
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    #[test]
-    fn test_read() {
-        let data = [0 /* ext_len */];
-        let mut cursor = std::io::Cursor::new(data);
-        let packet = AdPacket::read(&mut cursor).unwrap();
-        assert_eq!(packet.extensions.len(), 0);
+    mod packet {
+        use super::*;
+
+        #[test]
+        fn test_read() {
+            let data = [0 /* ext_len */];
+            let mut cursor = std::io::Cursor::new(data);
+            let packet = AdPacket::read(&mut cursor).unwrap();
+            assert_eq!(packet.extensions.len(), 0);
+        }
+
+        #[test]
+        fn test_write() {
+            let packet = AdPacket::new(AdExtensions::default());
+            let mut out = Vec::new();
+            let expected = [0];
+            packet.write(&mut out).unwrap();
+            assert_eq!(out, expected);
+        }
     }
 
-    #[test]
-    fn test_write() {
-        let packet = AdPacket::new(Vec::new());
-        let mut out = Vec::new();
-        let expected = [0];
-        packet.write(&mut out).unwrap();
-        assert_eq!(out, expected);
+    mod ext {
+        use super::*;
+
+        #[test]
+        fn test_tcp_read() {
+            let port: u16 = 14678;
+            let data = [
+                1, /* ext_len */
+                /* tcp extension */
+                AdExtensionType::TCP as _,
+                TCPAdExtension::SIZE,
+                ((port >> 8) & 0xff) as _,
+                (port & 0xff) as _,
+            ];
+            let mut cursor = std::io::Cursor::new(data);
+            let packet = AdPacket::read(&mut cursor).unwrap();
+            assert_eq!(packet.extensions.len(), 1);
+            match packet.extensions.tcp {
+                Some(TCPAdExtension { ad_port: port2 }) => assert_eq!(port2, port),
+                _ => assert!(false),
+            }
+        }
+
+        #[test]
+        fn test_tcp_write() {
+            let tcp_ext = TCPAdExtension { ad_port: 14678 };
+            let exts = AdExtensions {
+                tcp: Some(tcp_ext.clone()),
+            };
+            let expected = [
+                1, /* ext_len */
+                /* tcp extension */
+                AdExtensionType::TCP as _,
+                TCPAdExtension::SIZE,
+                ((tcp_ext.ad_port >> 8) & 0xff) as _,
+                (tcp_ext.ad_port & 0xff) as _,
+            ];
+            let mut out = Vec::new();
+            let packet = AdPacket::new(exts);
+            packet.write(&mut out).unwrap();
+            assert_eq!(&out, &expected);
+        }
     }
 }
