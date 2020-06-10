c2f6282b474d5a2e82a4091487cdc66ca4d779cd
anirudhb <anirudhb@users.noreply.github.com>
2020-06-10 11:10:40 -0700

    Port AdPacket to Deku

--- peardrop_protocol/src/ad.rs
+++ peardrop_protocol/src/ad.rs
@@ -1,138 +1,64 @@
-use super::{Marshal, Size};
-use thiserror::Error;
-
-/**
- * TCP acknowledgement packet extension.
- * See network extensions.
- */
-#[derive(Debug, Clone)]
-pub struct TCPAdExtension {
-    pub ad_port: u16,
-}
-
-impl Size for TCPAdExtension {
-    const SIZE: u8 = std::mem::size_of::<u16>() as _;
-}
-
-/**
- * Type of the advertisement packet extension.
- */
-#[repr(u8)]
-#[derive(num_derive::FromPrimitive, Debug, Copy, Clone, PartialEq, Eq)]
-pub enum AdExtensionType {
-    TCP,
-}
+use deku::prelude::*;
+use std::collections::HashSet;
 
 /**
  * Advertisement packet.
  * See the core protocol.
  */
-#[derive(Debug)]
+#[derive(Debug, DekuRead, DekuWrite)]
+#[deku(endian = "big")]
 pub struct AdPacket {
-    pub extensions: AdExtensions,
+    extensions_len: u8,
+    #[deku(
+        count = "extensions_len",
+        /* vec <-> hashset */
+        map = "|x: Vec<AdExtension>| -> Result<_, DekuError> { Ok(x.into_iter().collect::<HashSet<_>>()) }",
+        writer = "write_hashset(&self.extensions, output_is_le, field_bits)"
+    )]
+    pub extensions: HashSet<AdExtension>,
 }
 
-#[derive(Debug, Clone, Default)]
-pub struct AdExtensions {
-    pub tcp: Option<TCPAdExtension>,
+pub const TCP_EXTENSION_TYPE: u8 = 0;
+
+/* Hack because there is no write map for now */
+fn write_hashset(
+    x: &HashSet<AdExtension>,
+    output_is_le: bool,
+    bit_size: Option<usize>,
+) -> Result<BitVec<Msb0, u8>, DekuError> {
+    x.iter()
+        .cloned()
+        .collect::<Vec<_>>()
+        .write(output_is_le, bit_size)
 }
 
-impl AdExtensions {
-    pub fn len(&self) -> u8 {
-        let mut len = 0;
-        if self.tcp.is_some() {
-            len += 1;
-        }
-        len
-    }
-}
-
-impl Marshal for AdPacket {
-    type Error = AdPacketError;
-
-    /**
-     * Reads an AdPacket from the given reader.
-     */
-    fn read(r: &mut dyn std::io::Read) -> Result<Self, Self::Error> {
-        use byteorder::ReadBytesExt;
-        /* ext len */
-        let ext_len = r.read_u8()?;
-        let exts = Self::read_exts(r, ext_len)?;
-        Ok(Self::new(exts))
-    }
-
-    /**
-     * Writes an AdPacket to the given writer.
-     */
-    fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error> {
-        use byteorder::WriteBytesExt;
-        let ext_len = self.extensions.len() as u8;
-        w.write_u8(ext_len)?;
-        Self::write_exts(w, &self.extensions)?;
-        Ok(())
-    }
+/**
+ * Extension to an advertisement packet.
+ * See the core protocol.
+ */
+#[derive(Debug, Clone, Hash, PartialEq, Eq, DekuRead, DekuWrite)]
+#[deku(endian = "big", id_type = "u8")]
+pub enum AdExtension {
+    #[deku(id = "TCP_EXTENSION_TYPE")]
+    TCP { ad_port: u16 },
 }
 
 impl AdPacket {
     /**
      * Creates a new AdPacket using the given extensions.
      */
-    pub fn new(extensions: AdExtensions) -> Self {
-        Self { extensions }
-    }
-
-    fn read_exts(r: &mut dyn std::io::Read, ext_len: u8) -> Result<AdExtensions, AdPacketError> {
-        use byteorder::{ReadBytesExt, BE};
-        use num_traits::FromPrimitive;
-        let mut exts = AdExtensions::default();
-        for _ in 0..ext_len {
-            let ty = r.read_u8()?;
-            let ty = AdExtensionType::from_u8(ty).ok_or(AdExtensionError::InvalidType(ty))?;
-            use AdExtensionType::*;
-            match ty {
-                TCP => {
-                    let len = r.read_u8()?;
-                    if len != TCPAdExtension::SIZE {
-                        return Err(AdExtensionError::InvalidSize(ty, len).into());
-                    }
-                    let port = r.read_u16::<BE>()?;
-                    exts.tcp = Some(TCPAdExtension { ad_port: port });
-                }
-            }
+    pub fn new(extensions: HashSet<AdExtension>) -> Self {
+        Self {
+            extensions_len: extensions.len() as _,
+            extensions,
         }
-        Ok(exts)
     }
-
-    fn write_exts(w: &mut dyn std::io::Write, exts: &AdExtensions) -> Result<(), AdPacketError> {
-        use byteorder::{WriteBytesExt, BE};
-        if let Some(ref tcp) = exts.tcp {
-            w.write_u8(AdExtensionType::TCP as _)?;
-            w.write_u8(TCPAdExtension::SIZE)?;
-            w.write_u16::<BE>(tcp.ad_port)?;
-        }
-        Ok(())
-    }
-}
-
-#[derive(Error, Debug)]
-pub enum AdPacketError {
-    #[error("{0}")]
-    IO(#[from] std::io::Error),
-    #[error("{0}")]
-    AdExtension(#[from] AdExtensionError),
-}
-
-#[derive(Error, Debug)]
-pub enum AdExtensionError {
-    #[error("Invalid extension type {0}")]
-    InvalidType(u8),
-    #[error("Invalid extension size for {0:#?} (got {1})")]
-    InvalidSize(AdExtensionType, u8),
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use std::convert::{TryFrom, TryInto};
 
     mod packet {
         use super::*;
@@ -140,17 +66,15 @@ mod test {
         #[test]
         fn test_read() {
             let data = [0 /* ext_len */];
-            let mut cursor = std::io::Cursor::new(data);
-            let packet = AdPacket::read(&mut cursor).unwrap();
+            let packet = AdPacket::try_from(&data[..]).unwrap();
             assert_eq!(packet.extensions.len(), 0);
         }
 
         #[test]
         fn test_write() {
-            let packet = AdPacket::new(AdExtensions::default());
-            let mut out = Vec::new();
+            let packet = AdPacket::new(HashSet::new());
+            let out: Vec<u8> = packet.try_into().unwrap();
             let expected = [0];
-            packet.write(&mut out).unwrap();
             assert_eq!(out, expected);
         }
     }
@@ -164,38 +88,36 @@ mod test {
             let data = [
                 1, /* ext_len */
                 /* tcp extension */
-                AdExtensionType::TCP as _,
-                TCPAdExtension::SIZE,
+                TCP_EXTENSION_TYPE, /* type = tcp */
                 ((port >> 8) & 0xff) as _,
                 (port & 0xff) as _,
             ];
-            let mut cursor = std::io::Cursor::new(data);
-            let packet = AdPacket::read(&mut cursor).unwrap();
+            let packet = AdPacket::try_from(&data[..]).unwrap();
             assert_eq!(packet.extensions.len(), 1);
-            match packet.extensions.tcp {
-                Some(TCPAdExtension { ad_port: port2 }) => assert_eq!(port2, port),
-                _ => assert!(false),
-            }
+            assert!(packet
+                .extensions
+                .contains(&AdExtension::TCP { ad_port: port }));
         }
 
         #[test]
         fn test_tcp_write() {
-            let tcp_ext = TCPAdExtension { ad_port: 14678 };
-            let exts = AdExtensions {
-                tcp: Some(tcp_ext.clone()),
+            let ad_port = 14678;
+            let tcp_ext = AdExtension::TCP { ad_port };
+            let exts = {
+                let mut h = HashSet::new();
+                h.insert(tcp_ext);
+                h
             };
             let expected = [
                 1, /* ext_len */
                 /* tcp extension */
-                AdExtensionType::TCP as _,
-                TCPAdExtension::SIZE,
-                ((tcp_ext.ad_port >> 8) & 0xff) as _,
-                (tcp_ext.ad_port & 0xff) as _,
+                TCP_EXTENSION_TYPE, /* type = tcp */
+                ((ad_port >> 8) & 0xff) as _,
+                (ad_port & 0xff) as _,
             ];
-            let mut out = Vec::new();
             let packet = AdPacket::new(exts);
-            packet.write(&mut out).unwrap();
-            assert_eq!(&out, &expected);
+            let out: Vec<u8> = packet.try_into().unwrap();
+            assert_eq!(out, expected);
         }
     }
 }
--- peardrop_protocol/src/lib.rs
+++ peardrop_protocol/src/lib.rs
@@ -3,41 +3,10 @@
 mod ad;
 pub use ad::*;
 
+/*
 mod ack;
 pub use ack::*;
 
 mod sender;
 pub use sender::*;
-
-/**
- * Generic interface for marshalling objects.
- *
- * Why not Serde? Because we are not going through another format for encoding.
- * We're just writing this as bytes to a stream. Therefore each object
- * serializes itself differently.
- *
- * Should implement Debug.
- */
-pub trait Marshal: std::fmt::Debug {
-    type Error;
-    /**
-     * Marshal this object to a Write.
-     */
-    fn write(&self, w: &mut dyn std::io::Write) -> Result<(), Self::Error>;
-    /**
-     * Unmarshal this object from a Read.
-     */
-    fn read(r: &mut dyn std::io::Read) -> Result<Self, Self::Error>
-    where
-        Self: Sized;
-}
-
-/**
- * Generic interface for structs that have a predefined size.
- */
-pub trait Size {
-    /**
-     * Size of this struct.
-     */
-    const SIZE: u8;
-}
+*/
