ef77a5359f50839140dc8d51064644d171da6a10
anirudhb <anirudhb@users.noreply.github.com>
2020-06-16 17:45:11 -0700

    Add AdPacket bindings to C-API

--- peardrop_capi/peardrop.h
+++ peardrop_capi/peardrop.h
@@ -4,7 +4,10 @@
 #include <stdlib.h>
 
 typedef void ackpacket;
+
 typedef void acktype;
+
+typedef void adpacket;
 
 #ifdef __cplusplus
 extern "C" {
@@ -54,6 +57,25 @@ acktype *acktype_create_reject(uint8_t type_);
  * Frees an AckType.
  */
 void acktype_free(acktype *type_);
+
+/**
+ * Frees an AdPacket.
+ */
+void adpacket_free(adpacket *packet);
+
+/**
+ * Creates an AdPacket from the given buffer.
+ *
+ * Returns NULL on error.
+ */
+adpacket *adpacket_read(const uint8_t *buf, int32_t len);
+
+/**
+ * Writes an AdPacket to a buffer and returns it.
+ *
+ * Returns non-zero on error.
+ */
+int32_t adpacket_write(const adpacket *packet, uint8_t **out_buf, uintptr_t *out_len);
 
 #ifdef __cplusplus
 } // extern "C"
--- peardrop_capi/src/ad.rs
+++ peardrop_capi/src/ad.rs
@@ -1,32 +1,36 @@
 use super::*;
 
-/**
- * Creates an AdPacket from the given buffer.
- *
- * Returns NULL on error.
- */
+pub type adpacket = std::ffi::c_void;
+
+/// Creates an AdPacket from the given buffer.
+///
+/// Returns NULL on error.
 #[no_mangle]
-extern "C" fn adpacket_read(buf: *const u8, len: i32) -> *mut AdPacket {
+pub extern "C" fn adpacket_read(buf: *const u8, len: i32) -> *mut adpacket {
     let s_buf = unsafe { std::slice::from_raw_parts(buf, len as _) };
     let packet = AdPacket::from_buffer(s_buf);
     match packet {
         // TODO: Error handling
         Err(_) => std::ptr::null_mut(),
-        Ok(p) => Box::into_raw(Box::new(p)),
+        Ok(p) => Box::into_raw(Box::new(p)) as *mut _,
     }
 }
 
-/**
- * Writes an AdPacket to a buffer and returns it.
- *
- * Returns non-zero on error.
- */
+/// Writes an AdPacket to a buffer and returns it.
+///
+/// Returns non-zero on error.
 #[no_mangle]
-extern "C" fn adpacket_write(packet: *const AdPacket, out_buf: *mut *mut u8, out_len: *mut usize) -> i32 {
+pub extern "C" fn adpacket_write(packet: *const adpacket, out_buf: *mut *mut u8, out_len: *mut usize) -> i32 {
     if packet.is_null() || out_buf.is_null() || out_len.is_null() {
         return 1;
     }
-    let packet = unsafe { &*packet };
+    let packet = unsafe { &*(packet as *const AdPacket) };
     let v_buf = packet.to_buffer();
     write_vec(v_buf, out_buf, out_len)
+}
+
+/// Frees an AdPacket.
+#[no_mangle]
+pub extern "C" fn adpacket_free(packet: *mut adpacket) {
+    unsafe { Box::from_raw(packet as *mut AdPacket) };
 }
