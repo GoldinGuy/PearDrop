6461e22fd8f0481364c9c5806450b76d365364fe
Seth <sethgoldin27@gmail.com>
2020-06-25 02:41:47 -0400

    sliding panel for receiving updated

--- peardrop_flutter/lib/src/widgets/sliding_panel.dart
+++ peardrop_flutter/lib/src/widgets/sliding_panel.dart
@@ -6,9 +6,11 @@ import 'package:peardrop/src/widgets/linear_progress_indicator.dart';
 import 'package:peardrop/src/widgets/progress_indicator.dart';
 
 import '../home.dart';
+import '../home.dart';
 
 typedef void FileReceiveCallBack();
 typedef void ResetCallBack();
+typedef void SetPearPanelCallback(bool isOpen, PearPanel panel);
 
 class SlidingPanel extends StatelessWidget {
   SlidingPanel(
@@ -16,6 +18,7 @@ class SlidingPanel extends StatelessWidget {
       this.sc,
       this.fileName,
       this.pearPanel,
+      this.setPearPanel,
       this.accept,
       this.reset});
 
@@ -25,6 +28,7 @@ class SlidingPanel extends StatelessWidget {
   final FileReceiveCallBack accept;
   final PearPanel pearPanel;
   final ResetCallBack reset;
+  final SetPearPanelCallback setPearPanel;
 
   @override
   Widget build(BuildContext context) {
@@ -54,7 +58,7 @@ class SlidingPanel extends StatelessWidget {
                   children: [
                     CloseButton(
                       color: Colors.black,
-                      onPressed: () => {},
+                      onPressed: () => setPearPanel(false, PearPanel.accepting),
                     )
                   ],
                 ),
@@ -79,7 +83,7 @@ class SlidingPanel extends StatelessWidget {
                     mainAxisAlignment: MainAxisAlignment.start,
                     children: <Widget>[
                       Padding(
-                        padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                        padding: EdgeInsets.only(right: 5),
                         child: Icon(
                           Icons.description,
                           size: 20,
@@ -87,9 +91,10 @@ class SlidingPanel extends StatelessWidget {
                       ),
                       Text(
                         fileName,
+                        overflow: TextOverflow.ellipsis,
                         style: TextStyle(
                           fontWeight: FontWeight.w500,
-                          fontSize: 16.0,
+                          fontSize: 15.0,
                           color: Color(0xff559364),
                         ),
                       ),
@@ -184,64 +189,154 @@ class SlidingPanel extends StatelessWidget {
       return MediaQuery.removePadding(
           context: context,
           removeTop: true,
-          child: ListView(
-            controller: sc,
-            children: <Widget>[
-              // Row(
-              //   mainAxisAlignment: MainAxisAlignment.end,
-              //   children: [cancel],
-              // ),
-              Padding(
-                padding: EdgeInsets.all(20),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Receiving from " + peerDevice.getName(),
-                      style: TextStyle(
-                        fontWeight: FontWeight.normal,
-                        fontSize: 16.0,
-                      ),
-                    ),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.grey[200],
+              borderRadius: const BorderRadius.only(
+                  topLeft: Radius.circular(20.0),
+                  topRight: Radius.circular(20.0)),
+              boxShadow: <BoxShadow>[
+                BoxShadow(
+                  color: Colors.black12,
+                  blurRadius: 4.0,
+                  offset: const Offset(0.0, 4.0),
+                ),
+              ],
+            ),
+            child: ListView(
+              controller: sc,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    CloseButton(
+                      color: Colors.black,
+                      onPressed: () => setPearPanel(false, PearPanel.accepting),
+                    )
                   ],
                 ),
-              ),
-
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: <Widget>[
-                        Container(
-                          width: MediaQuery.of(context).size.width,
-                          padding: EdgeInsets.fromLTRB(0, 12, 0, 12),
-                          color: Colors.grey[50],
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
+                Padding(
+                  padding: EdgeInsets.only(left: 20, top: 0, bottom: 3),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "File Transfer",
+                        style: TextStyle(
+                          fontWeight: FontWeight.w600,
+                          fontSize: 19.0,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 20, top: 3, bottom: 15),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.only(right: 5),
+                        child: Icon(
+                          Icons.description,
+                          size: 20,
+                        ),
+                      ),
+                      Text(
+                        fileName,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w500,
+                          fontSize: 15.0,
+                          color: Color(0xff559364),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.only(bottom: 20, top: 15, left: 15, right: 15),
+                  child: PercentageProgressIndicator(),
+                ),
+                Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: 115.0,
+                    decoration: BoxDecoration(
+                      color: Colors.grey[50],
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(25.0),
+                          topRight: Radius.circular(25.0)),
+                      boxShadow: <BoxShadow>[
+                        BoxShadow(
+                          color: Colors.black12,
+                          blurRadius: 4.0,
+                          offset: const Offset(0.0, 4.0),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: EdgeInsets.all(20),
+                      child: Row(
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.end,
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
+                              Row(
+                                children: [
+                                  Text('Receiving from ',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w600,
+                                          fontSize: 14,
+                                          color: Colors.black)),
+                                  Text(peerDevice.getName(),
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 14,
+                                          color: Colors.black)),
+                                ],
+                              ),
+                              // TODO: display actual estimated time
                               Padding(
-                                padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                                child: Icon(
-                                  Icons.description,
-                                  size: 20,
-                                ),
-                              ),
-                              Center(
-                                child: Text(fileName,
-                                    style: TextStyle(fontSize: 14)),
-                              ),
+                                padding: EdgeInsets.only(top: 10, right: 10),
+                                child: Row(children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      color: Colors.grey[200],
+                                    ),
+                                    margin: EdgeInsets.only(right: 10),
+                                    padding: EdgeInsets.all(10),
+                                    child: Text('14s',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.w700,
+                                            fontSize: 14,
+                                            color: Colors.grey[600])),
+                                  ),
+                                  Text('2.3 Mb/s',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.w700,
+                                          fontSize: 14,
+                                          color: Colors.grey[600])),
+                                ]),
+                              )
                             ],
                           ),
-                        ),
-                        Padding(
-                          padding: EdgeInsets.all(10),
-                          child: LinearPercentageProgressIndicator(),
-                        )
-                      ]),
-                ],
-              ),
-            ],
+                          // TODO: determine whether icon is necessary or not
+                          // Padding(
+                          //     padding:
+                          //         EdgeInsets.only(left: 30, top: 5, right: 5),
+                          //     child: Icon(
+                          //       peerDevice.getIcon(),
+                          //       color: Color(0xff91c27d),
+                          //       size: 55,
+                          //     ))
+                        ],
+                      ),
+                    )),
+              ],
+            ),
           ));
     } else if (pearPanel == PearPanel.accepting) {
       return MediaQuery.removePadding(
