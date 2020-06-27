c521218dbdb6fa5ba60851b5b7fa9e0a905f441f
anirudhb <anirudhb@users.noreply.github.com>
2020-06-27 11:34:38 -0700

    Add UDP fork

--- peardrop_dart/ios/Classes/PeardropPlugin.m
+++ peardrop_dart/ios/Classes/PeardropPlugin.m
@@ -1,11 +1,11 @@
 #import "PeardropPlugin.h"
-#if __has_include(<peardrop/peardrop-Swift.h>)
-#import <peardrop/peardrop-Swift.h>
+#if __has_include(<libpeardrop/libpeardrop-Swift.h>)
+#import <libpeardrop/libpeardrop-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "peardrop-Swift.h"
+#import "libpeardrop-Swift.h"
 #endif
 
 @implementation PeardropPlugin
