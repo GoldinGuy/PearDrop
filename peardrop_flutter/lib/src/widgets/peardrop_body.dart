b0bc2d5e27e2726b5c4da2e3bb42a08ef48e6baf
Seth <sethgoldin27@gmail.com>
2020-07-03 22:06:54 -0400

    rework body

--- peardrop_flutter/lib/src/widgets/peardrop_body.dart
+++ peardrop_flutter/lib/src/widgets/peardrop_body.dart
@@ -31,10 +31,9 @@ class PearDropBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var deviceHeight = 25.0, containerHeight = 22.0;
+    var deviceHeight = 25.0;
     if (Platform.isWindows || Platform.isMacOS) {
       deviceHeight = 2.5;
-      containerHeight = 40.0;
     }
 
     return Container(
@@ -68,83 +67,251 @@ class PearDropBody extends StatelessWidget {
             alignment: Alignment.topRight,
           ),
           _getBody(),
-          Stack(
-            alignment: Alignment.topCenter,
-            children: <Widget>[
-              Padding(
-                padding: _getContainerPadding(),
-                child: Column(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width,
-                      //height: 150.0,
-                      decoration: BoxDecoration(
-                        color: Colors.grey[50],
-                        borderRadius: const BorderRadius.only(
-                            topLeft: Radius.circular(20.0),
-                            topRight: Radius.circular(20.0)),
-                        boxShadow: <BoxShadow>[
-                          BoxShadow(
-                            color: Colors.black12,
-                            blurRadius: 4.0,
-                            offset: const Offset(0.0, 4.0),
-                          ),
-                        ],
-                      ),
-                      child: SafeArea(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.only(top: containerHeight),
-                              child: Text(
-                                deviceName,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w700,
-                                  fontSize: 19,
-                                  color: Colors.black,
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(top: 3),
-                              child: InkWell(
-                                onTap: fileSelect,
-                                child: Container(
-                                  decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(15),
-                                    color: Colors.grey[200],
-                                  ),
-                                  padding: EdgeInsets.all(10),
-                                  margin: EdgeInsets.fromLTRB(15, 5, 15, 11),
-                                  child: _getFileContainer(),
-                                ),
-                              ),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(bottom: 4),
-                              child: Text(
-                                version,
-                                style: TextStyle(
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 13,
-                                  color: Colors.grey,
-                                ),
-                              ),
-                            )
-                          ],
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              _getPear()
-            ],
-          ),
+          _getInfoContainer(MediaQuery.of(context).size.width),
         ],
       ),
     );
+  }
+
+  // all methods below are related to UI based on if a file has been selected or not
+
+  Widget _getBody() {
+    if (!fileSelected) {
+      var deviceHeight = 23.0;
+      if (Platform.isWindows || Platform.isMacOS) {
+        deviceHeight = 15;
+      }
+
+      return Expanded(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(
+                top: deviceHeight,
+                left: 15,
+                right: 15,
+              ),
+              child: Text(
+                'Share With PearDrop',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                  fontSize: 23,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                top: 15,
+                left: 15,
+                right: 15,
+                bottom: deviceHeight,
+              ),
+              child: Text(
+                'Click below to start sharing, or begin from another nearby device',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(0, 8, 0, 3),
+              child: Container(
+                child: headers[Random().nextInt(headers.length)],
+                height: 288,
+              ),
+            ),
+          ],
+        ),
+      );
+    } else {
+      return Expanded(
+        child: Radar(
+          children: List.generate(devices.length, (i) {
+            return Container(
+              decoration: BoxDecoration(),
+              child: DeviceWidget(device: devices[i]),
+            );
+          }),
+        ),
+      );
+    }
+  }
+
+  Widget _getInfoContainer(double width) {
+    var containerHeight = 22.0;
+    if (Platform.isWindows || Platform.isMacOS) {
+      containerHeight = 40.0;
+    }
+
+    if (!fileSelected) {
+      return Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 0),
+            child: Column(
+              children: [
+                Container(
+                  width: width,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[50],
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4.0,
+                        offset: const Offset(0.0, 4.0),
+                      ),
+                    ],
+                  ),
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: containerHeight),
+                          child: Text(
+                            deviceName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 19,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 3),
+                          child: InkWell(
+                            onTap: fileSelect,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.grey[200],
+                              ),
+                              padding: EdgeInsets.all(10),
+                              margin: EdgeInsets.fromLTRB(15, 5, 15, 11),
+                              child: _getFileContainer(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 4),
+                          child: Text(
+                            version,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(),
+        ],
+      );
+    } else {
+      return Stack(
+        alignment: Alignment.topCenter,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.only(top: 50),
+            child: Column(
+              children: [
+                Container(
+                  width: width,
+                  //height: 150.0,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[50],
+                    borderRadius: const BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0)),
+                    boxShadow: <BoxShadow>[
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 4.0,
+                        offset: const Offset(0.0, 4.0),
+                      ),
+                    ],
+                  ),
+                  child: SafeArea(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: EdgeInsets.only(top: containerHeight),
+                          child: Text(
+                            deviceName,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w700,
+                              fontSize: 19,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(top: 3),
+                          child: InkWell(
+                            onTap: fileSelect,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(15),
+                                color: Colors.grey[200],
+                              ),
+                              padding: EdgeInsets.all(10),
+                              margin: EdgeInsets.fromLTRB(15, 5, 15, 11),
+                              child: _getFileContainer(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.only(bottom: 4),
+                          child: Text(
+                            version,
+                            style: TextStyle(
+                              fontWeight: FontWeight.w500,
+                              fontSize: 13,
+                              color: Colors.grey,
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            width: 85,
+            height: 85,
+            decoration:
+                ShapeDecoration(shape: CircleBorder(), color: Colors.white),
+            child: DecoratedBox(
+              decoration: ShapeDecoration(
+                shape: CircleBorder(),
+                image: DecorationImage(
+                  fit: BoxFit.cover,
+                  image: Image.asset('assets/images/icon.png').image,
+                ),
+              ),
+              // ),
+            ),
+          )
+        ],
+      );
+    }
   }
 
   Widget _getFileContainer() {
@@ -210,110 +377,6 @@ class PearDropBody extends StatelessWidget {
           ),
         ],
       );
-    }
-  }
-
-  // all methods below are related to UI based on if a file has been selected or not
-
-  Widget _getBody() {
-    if (!fileSelected) {
-      var deviceHeight = 23.0;
-      if (Platform.isWindows || Platform.isMacOS) {
-        deviceHeight = 15;
-      }
-
-      return Expanded(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                top: deviceHeight,
-                left: 15,
-                right: 15,
-              ),
-              child: Text(
-                'Share With PearDrop',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
-                  fontSize: 23,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.only(
-                top: 15,
-                left: 15,
-                right: 15,
-                bottom: deviceHeight,
-              ),
-              child: Text(
-                'Click below to start sharing, or begin from another nearby device',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 8, 0, 3),
-              child: Container(
-                child: getHeaderImage(),
-                height: 288,
-              ),
-            ),
-          ],
-        ),
-      );
-    } else {
-      return Expanded(
-        child: Radar(
-          children: List.generate(devices.length, (i) {
-            return Container(
-              decoration: BoxDecoration(
-                  //border: Border.all(),
-                  ),
-              child: DeviceWidget(device: devices[i]),
-            );
-          }),
-        ),
-      );
-    }
-  }
-
-  Image getHeaderImage() {
-    return headers[Random().nextInt(headers.length)];
-  }
-
-  Widget _getPear() {
-    if (!fileSelected) {
-      return Container();
-    } else {
-      return Container(
-        width: 85,
-        height: 85,
-        decoration: ShapeDecoration(shape: CircleBorder(), color: Colors.white),
-        child: DecoratedBox(
-          decoration: ShapeDecoration(
-            shape: CircleBorder(),
-            image: DecorationImage(
-              fit: BoxFit.cover,
-              image: Image.asset('assets/images/icon.png').image,
-            ),
-          ),
-          // ),
-        ),
-      );
-    }
-  }
-
-  EdgeInsets _getContainerPadding() {
-    if (!fileSelected) {
-      return EdgeInsets.only(top: 0);
-    } else {
-      return EdgeInsets.only(top: 50);
     }
   }
 }
