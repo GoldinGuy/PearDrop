e4f61b22c6c969161403c083e29bbbbb4571e1ab
Seth <sethgoldin27@gmail.com>
2020-07-03 22:32:51 -0400

    center panel content

--- peardrop_flutter/lib/src/widgets/sliding_panel.dart
+++ peardrop_flutter/lib/src/widgets/sliding_panel.dart
@@ -17,107 +17,109 @@ class SlidingPanel extends StatelessWidget {
     return MediaQuery.removePadding(
       context: context,
       removeTop: true,
-      child: ListView(
-        controller: sc,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.all(30),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  '$deviceName would like to share',
-                  style: TextStyle(
-                    fontWeight: FontWeight.normal,
-                    fontSize: 16.0,
-                  ),
-                ),
-              ],
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: <Widget>[
-              Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      child: Center(
+        child: ListView(
+          controller: sc,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(30),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      color: Colors.grey[200],
-                    ),
-                    padding: EdgeInsets.all(10),
-                    margin: EdgeInsets.fromLTRB(15, 5, 15, 16),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        Padding(
-                          padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
-                          child: Icon(
-                            Icons.description,
-                            size: 20,
-                          ),
-                        ),
-                        Expanded(
-                          child: Text(
-                            file?.filename ?? '',
-                            overflow: TextOverflow.ellipsis,
-                            style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 14,
-                              color: Color(0xff559364),
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        ),
-                      ],
+                  Text(
+                    '$deviceName would like to share',
+                    style: TextStyle(
+                      fontWeight: FontWeight.normal,
+                      fontSize: 16.0,
                     ),
                   ),
-                  Container(
-                    width: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.fromLTRB(40, 17, 40, 5),
-                    child: GestureDetector(
-                      onTap: accept,
-                      child: Container(
-                        width: 80,
-                        height: 45,
-                        decoration: BoxDecoration(
-                          gradient: LinearGradient(
-                            colors: [
-                              Color(0xff91c27d),
-                              Color(0xff559364),
-                            ],
-                            begin: Alignment.topLeft,
-                            end: Alignment.bottomRight,
+                ],
+              ),
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(15),
+                        color: Colors.grey[200],
+                      ),
+                      padding: EdgeInsets.all(10),
+                      margin: EdgeInsets.fromLTRB(15, 5, 15, 16),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Padding(
+                            padding: EdgeInsets.fromLTRB(0, 0, 5, 0),
+                            child: Icon(
+                              Icons.description,
+                              size: 20,
+                            ),
                           ),
-                          borderRadius: BorderRadius.circular(20),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.black12,
-                              offset: Offset(5, 5),
-                              blurRadius: 10,
-                            )
-                          ],
-                        ),
-                        child: Center(
-                          child: Text(
-                            'Accept',
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18,
-                              fontWeight: FontWeight.w500,
+                          Expanded(
+                            child: Text(
+                              file?.filename ?? '',
+                              overflow: TextOverflow.ellipsis,
+                              style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 14,
+                                color: Color(0xff559364),
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      width: MediaQuery.of(context).size.width,
+                      padding: EdgeInsets.fromLTRB(40, 17, 40, 5),
+                      child: GestureDetector(
+                        onTap: accept,
+                        child: Container(
+                          width: 80,
+                          height: 45,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              colors: [
+                                Color(0xff91c27d),
+                                Color(0xff559364),
+                              ],
+                              begin: Alignment.topLeft,
+                              end: Alignment.bottomRight,
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.black12,
+                                offset: Offset(5, 5),
+                                blurRadius: 10,
+                              )
+                            ],
+                          ),
+                          child: Center(
+                            child: Text(
+                              'Accept',
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.w500,
+                              ),
                             ),
                           ),
                         ),
                       ),
                     ),
-                  ),
-                ],
-              ),
-            ],
-          ),
-        ],
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
